a549fb16bd5606c8689157a9d27dc70ed6ca0b16
wiktorpp <grazynapp65@gmail.com>
2021-08-17 10:50:05 +0200

    Working test (forgot to commit ealier)

--- termbud.py
+++ termbud.py
@@ -1,37 +1,70 @@
 import curses
+import random
 
 class Termbud:
     def __init__(self):
+        global scr
         scr = curses.initscr()
+        curses.noecho()
+        self.position = 9
+        self.time = 0
+        self.health = 1.0
 
     def __str__(self):
         pass
 
-    def generate(self=None, blink=False, pet=False, bigEyes=False, blushing=False, squinting=False):
-        if not bigEyes:
-            eye = "o"
-        else:
-            eye = "O"
-        if blink:
-            eye = "-"
-        if squinting:
-            eye = "^"
+    def generate(self=None, blush=" ", eye="o", mouth="w", pet=False):
 
-        if not blushing:
-            blush = " "
-        else:
-            blush = "*"
-        mouth = "w"
-        output = ""
-        output += f"┌─────────────────────┐\n"
-        output += f"│                     │\n"
-        output += f"│        {blush}{eye}{mouth}{eye}{blush}        │\n"
-        output += f"│                     │\n"
-        output += f"└─────────────────────┘\n"
-        return output
+        self.print_box()
+        self.update()
+
+        if pet:
+            scr.addstr(1, self.position, "  _/ ")
+        bud = f"{blush}{eye}{mouth}{eye}{blush}"
+
+        scr.addstr(2, self.position, bud)
+
+    def update(self):
+        if self.time % 5 == 0:
+            self.position = self.position + random.randint(-1, 1)
+            if self.position <= 1:
+                self.position = 2
+            if self.position >= 17:
+                self.position = 16
+        self.time += 1
+
+    def print_box(self):
+        scr.addstr(0, 0, "┌─────────────────────┐")
+        scr.addstr(1, 0, "│                     │")
+        scr.addstr(2, 0, "│                     │")
+        scr.addstr(3, 0, "│                     │")
+        scr.addstr(4, 0, "│                     │")
+        scr.addstr(5, 0, "└─────────────────────┘")
 
 if __name__ == "__main__":
-    print(Termbud.generate())
-    print(Termbud.generate(squinting=True))
-    print(Termbud.generate(blushing=True))
-    import pdb; pdb.set_trace()
+    """
+    t = Termbud()
+    t.print_box()
+    t.generate(eye="^")
+    scr.getkey()
+    t.generate(blush="*")
+    scr.getkey()
+    t.generate(pet=True, eye="O", blush="*")
+    scr.getkey()
+    """
+    t = Termbud()
+    pet_level = 0
+    t.generate()
+    while True:
+        button = scr.getkey()
+        scr.addstr(0, 0, repr(button))
+        if button == "q" or button == "\x1b":
+            exit()
+        else:
+            pet_level += 1
+        if pet_level == 0:
+            t.generate()
+        elif pet_level == 1:
+            t.generate(pet=True, eye="^")
+        elif pet_level == 5:
+            t.generate(pet=True, eye="O", blush="*")
